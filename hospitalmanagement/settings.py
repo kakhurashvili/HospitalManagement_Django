c47189cacc3a51956e6bedaacdb003ba67d0571a
shako kakhurashvili <kakhurashvilis@gmail.com>
2022-03-08 13:22:39 +0400

    idle session remove

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -155,8 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from datetime import timedelta
 
 AUTO_LOGOUT = {
-    'IDLE_TIME': timedelta(minutes=1),
-    'SESSION_TIME': timedelta(minutes=5),
+    'SESSION_TIME': timedelta(minutes=1),
     'MESSAGE': 'ვიზიტის დრო ამოიწურა!',
     'REDIRECT_TO_LOGIN_IMMEDIATELY': False,
 }
